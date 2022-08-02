ef42baeae00ad83a0b74fbdfcfa5e67d4b302e1c
Slav <Sl-aj@yandex.ua>
2022-08-02 13:35:24 +0300

    add TransactionDTO

---
+++ ETL/DTO/TransactionDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL.DTO
+{
+    public class TransactionDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public decimal Payment { get; set; }
+        public DateTime Date { get; set; }
+        public long AccountNumber { get; set; }
+        public string Service { get; set; }
+    }
+}
